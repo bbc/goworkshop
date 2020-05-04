8a96e0cb27fea5624e3348e998394761a928af10
Beth Anderson <beth.anderson@bbc.co.uk>
2020-05-04 14:25:46 +0100

    Add main implementation

--- goal1/main.go
+++ goal1/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
-func main() {
+// EncodeRequest represents a request to encode
+type EncodeRequest struct {
+	URL string
+}
 
+// EncodeResponse represents the result of encoding
+type EncodeResponse struct {
+	ShortURL string
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
